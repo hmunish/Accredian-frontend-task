23e8d56baa074a26e7f3055811cc72e645b7e46e
Munish <hmunish5@gmail.com>
2023-12-17 18:54:43 +0200

    Add async thunks for sign--up & login

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -6,10 +6,15 @@ import {
   removeLocalStorageAuthToken,
   setLocalStorageAuthToken,
 } from "../../utility/helper";
+import sanitize from "../../utility/sanitize";
 
 const initialState = {
-  isLoading: false,
-  isError: null,
+  isLoadingSignUp: false,
+  isErrorSignUp: null,
+  isSuccessSignUp: false,
+  isLoadingLogin: false,
+  isErrorLogin: null,
+  isSuccessLogin: false,
   isAuthorized: false,
   userAuthToken: getLocalStorageAuthToken(),
 };
@@ -31,46 +36,113 @@ export const authorizeUser = createAsyncThunk(
   }
 );
 
+export const signup = createAsyncThunk(
+  "onboard/signup",
+  async ({ username, email, password }, thunkAPI) => {
+    try {
+      const sanitizedUsername = sanitize(username);
+      const sanitizedEmail = sanitize(email);
+      const sanitizedPassword = sanitize(password);
+
+      // Making request to create new user
+      const response = await axios.post(`${API_URL}/user/sign-up`, {
+        username: sanitizedUsername,
+        email: sanitizedEmail,
+        password: sanitizedPassword,
+      });
+
+      // Confirming if response code is 201(created) else throwing error
+      if (response.status !== 201) throw new Error("Error creating user");
+
+      // Resolving the function with success value
+      return thunkAPI.fulfillWithValue(response.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data.message || error.message
+      );
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  "onboard/signin",
+  async ({ email, password }, thunkAPI) => {
+    try {
+      const sanitizedEmail = sanitize(email);
+      const sanitizedPassword = sanitize(password);
+
+      // Making request to create new user
+      const response = await axios.post(`${API_URL}/user/login`, {
+        email: sanitizedEmail,
+        password: sanitizedPassword,
+      });
+
+      // Confirming if response code is 200(OK) else throwing error
+      if (response.status !== 200) throw new Error("Error signing in the user");
+
+      // Resolving the function with success value
+      return thunkAPI.fulfillWithValue(response.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data.message || error.message
+      );
+    }
+  }
+);
+
 const userSlice = createSlice({
   name: "userSlice",
   initialState,
-  // reducers: {
-  //   updateAuthorization(state, action) {
-  //     state.isLoading = false;
-  //     state.isError = null;
-  //     state.isAuthorized = true;
-  //     state.userAuthToken = action.payload.authorization;
-  //     state.profile = action.payload.user;
-  //     axios.defaults.headers.common.Authorization = state.userAuthToken;
-  //     setLocalStorageAuthToken(state.userAuthToken);
-  //   },
-  //   removeAuthorization(state) {
-  //     removeLocalStorageAuthToken();
-  //     axios.defaults.headers.common.Authorization = undefined;
-  //     state.isLoading = false;
-  //     state.isError = null;
-  //     state.isAuthorized = false;
-  //     state.userAuthToken = getLocalStorageAuthToken();
-  //     state.profile = {};
-  //   },
-  // },
+  reducers: {
+    removeAuthorization(state) {
+      removeLocalStorageAuthToken();
+      state.isLoading = false;
+      state.isError = null;
+      state.isAuthorized = false;
+      state.userAuthToken = getLocalStorageAuthToken();
+    },
+  },
   extraReducers: (builder) => {
-    builder.addCase(authorizeUser.fulfilled, (state, action) => {
+    builder.addCase(authorizeUser.fulfilled, (state) => {
       state.isLoading = false;
       state.isError = null;
       state.isAuthorized = true;
-      state.profile = action.payload;
     });
-    builder.addCase(authorizeUser.pending, (state) => {
-      state.isLoading = true;
-    });
-    builder.addCase(authorizeUser.rejected, (state, action) => {
-      state.isLoading = false;
+    builder.addCase(authorizeUser.rejected, (state) => {
       state.isAuthorized = false;
-      state.isError = action.payload;
+    });
+    builder.addCase(signup.fulfilled, (state, action) => {
+      state.isSuccessSignUp = true;
+      state.isErrorSignUp = null;
+      state.isLoadingSignUp = false;
+    });
+    builder.addCase(signup.pending, (state) => {
+      state.isLoadingSignUp = true;
+    });
+    builder.addCase(signup.rejected, (state, action) => {
+      state.isSuccessSignUp = false;
+      state.isErrorSignUp = action.payload;
+      state.isLoadingSignUp = false;
+      state.isAuthorized = false;
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.isSuccessLogin = true;
+      state.isErrorLogin = null;
+      state.isLoadingLogin = false;
+      state.isAuthorized = true;
+      setLocalStorageAuthToken(action.payload.authorization);
+    });
+    builder.addCase(login.pending, (state) => {
+      state.isLoadingLogin = true;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      state.isSuccessLogin = false;
+      state.isErrorLogin = action.payload;
+      state.isLoadingLogin = false;
+      state.isAuthorized = false;
     });
   },
 });
 
-// export const { updateAuthorization, removeAuthorization } = userSlice.actions;
+export const { removeAuthorization } = userSlice.actions;
 export default userSlice.reducer;
